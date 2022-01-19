2cf52d3357fdcb0c3afa65401a1212aaf6f1571e
yshimazu <shimazuyuki7@gmail.com>
2022-01-19 16:54:06 +0900

    fix_01_ex03_segfo

--- cpp01/ex03/HumanB.cpp
+++ cpp01/ex03/HumanB.cpp
@@ -1,12 +1,17 @@
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name) : _name(name) {}
+HumanB::HumanB(std::string name) : _name(name), _weapon(NULL) {}
 
 HumanB::~HumanB() {}
 
 void HumanB::attack()
 {
-    std::cout << _name << " attacks with his " << _weapon->getType() << std::endl;
+    std::cout << _name << " attacks with his ";
+    if (_weapon)
+        std::cout << _weapon->getType();
+    else
+        std::cout << "fists";
+    std::cout << std::endl;
 }
 
 void HumanB::setWeapon(Weapon& weapon)
