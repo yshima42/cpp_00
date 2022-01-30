13c4dfe226967f03b02d62428f7ad1d59ede33b6
yshimazu <shimazuyuki7@gmail.com>
2022-01-30 20:24:32 +0900

    add_new_handler

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -1,7 +1,14 @@
 #include "Zombie.hpp"
 
+void outOfMem()
+{
+    std::cerr << "Error: Out of memory" << std::endl;
+    std::abort();
+}
+
 int main()
 {
+    std::set_new_handler(outOfMem); 
     int N = 5;
     Zombie *zombies = zombieHorde(N, "hZombie");
     while (N--)
