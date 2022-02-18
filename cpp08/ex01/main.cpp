9a9fd1220f5f63c0537f2c65408c37f7d67f656f
yshimazu <shimazuyuki7@gmail.com>
2022-02-18 18:18:09 +0900

    add

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -3,6 +3,7 @@
 
 void testSubject() {
   try {
+
     std::cout << YELLOW << "---testSubject---" << RESET << std::endl;
     Span sp = Span(5);
     sp.addNumber(5);
@@ -13,6 +14,7 @@ void testSubject() {
     std::cout << sp.shortestSpan() << std::endl;
     std::cout << sp.longestSpan() << std::endl;
     std::cout << std::endl;
+
   } catch (std::exception& e) {
     std::cout << RED << e.what() << RESET << std::endl;
   }
@@ -20,15 +22,19 @@ void testSubject() {
 
 void testError() {
   std::cout << YELLOW << "---testError---" << RESET << std::endl;
+
   try {
+
     Span sp = Span(5);
     sp.addNumber(5);
     std::cout << sp.shortestSpan() << std::endl;
+
   } catch (std::exception& e) {
     std::cout << RED << e.what() << RESET << std::endl;
   }
 
   try {
+
     Span sp = Span(5);
     sp.addNumber(5);
     std::cout << sp.longestSpan() << std::endl;
@@ -38,6 +44,7 @@ void testError() {
   }
 
   try {
+
     Span sp = Span(5);
     sp.addNumber(5);
     sp.addNumber(3);
@@ -53,6 +60,7 @@ void testError() {
 }
 
 void testIter() {
+  std::cout << YELLOW << "---testIter---" << RESET << std::endl;
   try {
 
     int arr[5] = {2, 9, 3, 8, 4};
@@ -65,10 +73,29 @@ void testIter() {
   } catch (std::exception& e) {
     std::cout << RED << e.what() << RESET << std::endl;
   }
+  std::cout << std::endl;
+}
+
+void testIterError() {
+  std::cout << YELLOW << "---testIterError---" << RESET << std::endl;
+  try {
+
+    int arr[6] = {2, 9, 3, 8, 4, 5};
+    std::vector<int> vec(arr, arr + 6);
+    Span sp = Span(5);
+    sp.addNumber(vec.begin(), vec.end());
+    std::cout << sp.shortestSpan() << std::endl;
+    std::cout << sp.longestSpan() << std::endl;
+
+  } catch (std::exception& e) {
+    std::cout << RED << e.what() << RESET << std::endl;
+  }
+  std::cout << std::endl;
 }
 
 int main() {
   testSubject();
   testError();
   testIter();
+  testIterError();
 }
